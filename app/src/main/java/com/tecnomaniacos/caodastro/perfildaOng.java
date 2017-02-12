42eb7a4e4a6bc8bf7cdc5d8e11229ddd798f388d
ludimila3008 <luluhatz@hotmail.com>
2017-02-12 18:44:43 -0200

    COMMIT COM TUDO ATUALIZADO (12/02)

--- app/src/main/java/com/tecnomaniacos/caodastro/perfildaOng.java
+++ app/src/main/java/com/tecnomaniacos/caodastro/perfildaOng.java
@@ -3,13 +3,40 @@ package com.tecnomaniacos.caodastro;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.TextView;
+import android.widget.TextView;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class perfildaOng extends AppCompatActivity {
+    TextView textView8;
+    TextView textView9;
+    TextView textView10;
+    TextView textView11;
+    TextView textView7;
+    Button buttoncadastrocaes;
+    ImageView imageView5;
+    Toolbar toolbarperfil;
+    ImageView imageView4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfilda_ong);
+        textView8= (TextView) findViewById(R.id.textView8);
+        textView9=(TextView)findViewById(R.id.textView9);
+        textView10=(TextView)findViewById(R.id.textView10);
+        textView11=(TextView)findViewById(R.id.textView11);
+        textView7=(TextView)findViewById(R.id.textView7);
+        buttoncadastrocaes=(Button)findViewById(R.id.buttoncadastrocaes);
+        imageView5=(ImageView)findViewById(R.id.imageView5);
+        toolbarperfil=(Toolbar)findViewById(R.id.toolbarperfil);
+        imageView4=(ImageView)findViewById(R.id.imageView4);
 
 
 
--- app/src/main/java/com/tecnomaniacos/caodastro/perfildospais.java
+++ app/src/main/java/com/tecnomaniacos/caodastro/perfildospais.java
@@ -1,29 +1,46 @@
 package com.tecnomaniacos.caodastro;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.TextView;
+import android.widget.TextView;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class perfildospais extends AppCompatActivity {
+    Toolbar toolbarperfilpais;
+    TextView textViewnumerotelefone;
+    TextView textViewcep;
+    TextView textViewnumerocep;
+    Button cadastrocrianças;
+    ImageView imageView4;
+    TextView textViewnome;
+    TextView textViewtelefone;
+    ImageView imageViewautismo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfildospais);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        textViewnumerotelefone= (TextView) findViewById(R.id.textViewnumerotelefone);
+        textViewcep=(TextView)findViewById(R.id.textViewcep);
+        textViewnumerocep=(TextView)findViewById(R.id.textViewnumerocep);
+        textViewtelefone=(TextView)findViewById(R.id.textViewtelefone);
+        textViewnome=(TextView)findViewById(R.id.textViewnome);
+        cadastrocrianças=(Button)findViewById(R.id.buttoncadastrocrianças);
+        imageViewautismo=(ImageView)findViewById(R.id.imageViewautismo);
+        toolbarperfilpais=(Toolbar)findViewById(R.id.toolbarperfilpais);
+        imageView4=(ImageView)findViewById(R.id.imageView4);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
 
-}
+
+
+
+    }}
